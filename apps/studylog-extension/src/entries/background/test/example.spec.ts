f02140f7ffddabe18283edda5b1d30bb07b4a2a0
Gal Zafar <theycallmefachfach@gmail.com>
2022-12-13 20:25:29 +0200

    added vitest support to the extension

---
+++ apps/studylog-extension/src/entries/background/test/example.spec.ts
@@ -0,0 +1,8 @@
+import { describe, it, assert, expect } from 'vitest';
+// vitest example test
+
+describe('suite name', () => {
+    it('foo', () => {
+        assert.equal(Math.sqrt(4), 2)
+    })
+})
--- apps/studylog-extension/vite.config.ts
+++ apps/studylog-extension/vite.config.ts
@@ -6,6 +6,7 @@ import { getManifest } from "./src/manifest";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  test: { };
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
