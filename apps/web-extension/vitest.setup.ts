ae8bc923fe28432ce431e54be4c9d6ec06873b3d
ohmree <13455401+ohmree@users.noreply.github.com>
2022-12-14 21:26:06 +0200

    Auto-mock everything that looks like an event listener

--- apps/web-extension/vitest.setup.ts
+++ apps/web-extension/vitest.setup.ts
@@ -1,27 +1,25 @@
 import { beforeAll, vi } from 'vitest';
 import type { Browser } from 'webextension-polyfill';
 
-const addListenerMock = vi.fn().mockImplementation(_ => {});
-
-const mockBrowser = {
-  tabs: {
-    onUpdated: {
-      addListener: addListenerMock,
-    },
-    onActivated: {
-      addListener: addListenerMock,
-    },
-    onRemoved: {
-      addListener: addListenerMock,
-    },
-    onCreated: {
-      addListener: addListenerMock,
-    },
-  },
-};
-
 beforeAll(() => {
   // (globalThis as any).chrome = { runtime: { id: 'TESTING' } };
   vi.stubGlobal('chrome', { runtime: { id: 'TESTING' } });
-  vi.mock('webextension-polyfill', () => mockBrowser);
+  vi.mock('webextension-polyfill', () => {
+    const addListenerMock = vi
+      .fn()
+      .mockImplementation((..._args: unknown[]) => {});
+    const tabs = new Proxy(
+      {},
+      {
+        get(target, prop, receiver) {
+          const matches = prop.toString().match(/^fon([A-Z][A-Za-z]*)$/);
+          if (matches == null) {
+            return Reflect.get(target, prop, receiver);
+          }
+          return { addListener: addListenerMock };
+        },
+      },
+    );
+    return { tabs } as Browser;
+  });
 });
