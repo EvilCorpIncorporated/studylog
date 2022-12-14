09b83b6203122f47326dfd65a7d7555b36fbd3d8
ohmree <13455401+ohmree@users.noreply.github.com>
2022-12-14 18:08:23 +0200

    Move `manifest.ts` one level up and improve types
    Most notably we now no longer depend on @types/chrome

--- apps/web-extension/vite.config.ts
+++ apps/web-extension/vite.config.ts
@@ -2,17 +2,17 @@ import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import webExtension from '@samrum/vite-plugin-web-extension';
 import path from 'path';
-import { getManifest } from './src/manifest';
+import { getManifest } from './manifest';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  test: { };
+
   return {
     plugins: [
       vue(),
       webExtension({
-        manifest: getManifest(Number(env.MANIFEST_VERSION)),
+        manifest: getManifest(Number(env['MANIFEST_VERSION'])),
       }),
     ],
     resolve: {
