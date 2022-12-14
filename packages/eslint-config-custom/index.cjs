ad9ce02c8d5738c1765c0413930a38a048c40b62
ohmree <13455401+ohmree@users.noreply.github.com>
2022-12-14 14:35:07 +0200

    Rename `eslint-config-custom` to `eslint-config`

--- packages/eslint-config-custom/index.cjs
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('eslint-define-config');
-
-module.exports = defineConfig({
-  extends: ['turbo', '@nuxt', 'prettier'],
-  rules: {
-    // "@next/next/no-html-link-for-pages": "off",
-    // "react/jsx-key": "off",
-  },
-});
