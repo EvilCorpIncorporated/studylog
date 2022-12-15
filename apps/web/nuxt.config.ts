2ba5c2ae9a507941e05872c8d7497253ca17d9fa
ohmree <13455401+ohmree@users.noreply.github.com>
2022-12-15 15:19:25 +0200

    Comment out nuxt-prisma
    It doesn't seem to work at the moment.

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['@sidebase/nuxt-prisma'],
+  // extends: ['@sidebase/nuxt-prisma'],
   typescript: {
     shim: false,
   },
