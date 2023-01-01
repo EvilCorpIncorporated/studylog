f3e15acc35ea5568faf88f92a0fe7ae2eedc1765
Gal Zafar <theycallmefachfach@gmail.com>
2023-01-01 09:21:46 +0200

    make trpc router easier to read; run linter;

--- packages/trpc/src/routers/index.ts
+++ packages/trpc/src/routers/index.ts
@@ -3,36 +3,36 @@ import e from '@studylog/edgedb';
 import type { Client } from 'edgedb';
 import { publicProcedure, router } from '..';
 
+const zod_input = z.object({
+  user_id: z.string(),
+  events: z.array(
+    z.object({
+      enter_time: z.date({ coerce: true }),
+      exit_time: z.date({ coerce: true }),
+    }),
+  ),
+});
+
+async function _mutation(edgedb: Client, req) {
+  const { events, user_id } = req.input;
+  const query = e.params({ events: e.json }, $ => {
+    return e.for(e.json_array_unpack($.events), event => {
+      return e.insert(e.Event, {
+        exit_time: e.cast(e.datetime, event.exit_time),
+        enter_time: e.cast(e.datetime, event.enter_time),
+        user: e.insert(e.User, { user_id }),
+      });
+    });
+  });
+  const result = await query.run(edgedb, { events });
+  return result;
+}
+
 export function createAppRouter(edgedb: Client) {
   return router({
-    addEvents: publicProcedure
-      .input(
-        z.object({
-          user_id: z.string(),
-          events: z.array(
-            z.object({
-              enter_time: z.date({ coerce: true }),
-              exit_time: z.date({ coerce: true }),
-            }),
-          ),
-        }),
-      )
-      .mutation(async req => {
-        const { events, user_id } = req.input;
-        const query = e.params({ events: e.json }, $ => {
-          return e.for(e.json_array_unpack($.events), event => {
-            return e.insert(e.Event, {
-              // @ts-expect-error FIXME: we need to figure out the correct way to cast this json to a datetime.
-              exit_time: e.cast(e.datetime, event.exit_time),
-              // @ts-expect-error FIXME: we need to figure out the correct way to cast this json to a datetime.
-              enter_time: e.cast(e.datetime, event.enter_time),
-              user: e.insert(e.User, { user_id }),
-            });
-          });
-        });
-        const result = await query.run(edgedb, { events });
-        return result;
-      }),
+    addEvents: publicProcedure.input(zod_input).mutation( async (req) => {
+      return await _mutation(edgedb, req);
+    } ),
   });
 }
 
